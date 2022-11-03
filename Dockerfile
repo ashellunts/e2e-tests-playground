04bd41f153749207ae9e567cf025a28e4accd8de
Artur Shellunts <shellunts.artur@gmail.com>
2022-11-03 17:55:35 +0100

    run offer in test

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM golang:1.17-alpine3.13
 
 ENV CGO_ENABLED=0
+RUN go install github.com/pion/webrtc/v3/examples/pion-to-pion/offer@latest
+
 RUN mkdir /app
 WORKDIR /app
 ADD go.mod go.sum /app/
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os/exec"
 	"testing"
 
@@ -8,42 +9,11 @@ import (
 )
 
 func Test(t *testing.T) {
-
-	cmd, err := RunCommand("echo", "1")
-
 	r := require.New(t)
-	r.NotNil(cmd)
-	r.Nil(err)
 
-	cmd_err := cmd.Wait()
+	cmd := exec.Command("offer")
+	output, err := cmd.CombinedOutput()
+	fmt.Println(string(output))
 
-	r.Nil(cmd_err)
-
-	// program_output := make([]byte, 0)
-	// for {
-	// 	tmp := make([]byte, 4)
-	// 	n, err := stdoutPipe.Read(tmp)
-	// 	if err != nil {
-	// 		break
-	// 	}
-	// 	program_output = append(program_output, tmp[0:n]...)
-	// }
-
-	// expected := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x0a}
-	// a.Equal(expected, program_output)
-}
-
-func RunCommand(name string, arg ...string) (*exec.Cmd, error) {
-	cmd := exec.Command(name, arg...)
-
-	// stdoutPipe, err := cmd.StdoutPipe()
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	if err := cmd.Start(); err != nil {
-		return nil, err
-	}
-
-	return cmd, nil
+	r.Error(err)
 }
