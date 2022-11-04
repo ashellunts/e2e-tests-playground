acc5edc391c6b1480031df48286a75d84b745ecb
Artur Shellunts <shellunts.artur@gmail.com>
2022-11-04 12:56:56 +0100

    asfasdf

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 docker compose -f sut/docker-compose.yml up -d
 
 timeout 10 docker compose -f sut/docker-compose.yml logs -f | grep -q "answer  | Message from DataChannel"
-timeout 10 docker compose -f sut/docker-compose.yml logs -f | grep -q "offerh   | Message from DataChannel"
+timeout 10 docker compose -f sut/docker-compose.yml logs -f | grep -q "offer   | Message from DataChannel"
 
 docker compose -f sut/docker-compose.yml stop
 docker compose -f sut/docker-compose.yml rm -f
