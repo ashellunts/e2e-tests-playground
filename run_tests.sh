3e542f97a4781b01366fd0c5f4c9ed4c5a046c9a
Artur Shellunts <artur.shellunts@profiforms.de>
2022-11-04 13:14:38 +0100

    safjsafkd

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,19 @@
+
 docker compose -f sut/docker-compose.yml up -d
 
 timeout 10 docker compose -f sut/docker-compose.yml logs -f | grep -q "answer  | Message from DataChannel"
+xx=$?
 timeout 10 docker compose -f sut/docker-compose.yml logs -f | grep -q "offer   | Message from DataChannel"
+yy=$?
 
 docker compose -f sut/docker-compose.yml stop
 docker compose -f sut/docker-compose.yml rm -f
+
+if (( $xx == 0 && $yy == 0))
+then
+    echo "PASS"
+    exit 0
+else
+    echo "FAIl"
+    exit 1
+fi
