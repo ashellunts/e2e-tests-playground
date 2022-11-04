ed2dbd312dce14b519afe0c0810e4da9715b49b6
Artur Shellunts <shellunts.artur@gmail.com>
2022-11-04 08:46:57 +0100

    add offer/asnwer source code

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,10 @@ RUN go install github.com/pion/webrtc/v3/examples/pion-to-pion/answer@latest
 RUN mkdir /app
 WORKDIR /app
 ADD go.mod go.sum /app/
+COPY offer /app/offer
+COPY answer /app/answer
 ADD *.go /app/
-RUN go test -timeout 3s .
+RUN go build -o offer_app ./offer
+RUN go build -o answer_app ./answer
+
+RUN go test -v -timeout 10s .
--- main_test.go
+++ main_test.go
@@ -12,23 +12,27 @@ import (
 func Test(t *testing.T) {
 	r := require.New(t)
 
-	cmd := exec.Command("answer")
+	cmd := exec.Command("./answer_app")
 	var outb, errb bytes.Buffer
 	cmd.Stdout = &outb
 	cmd.Stderr = &errb
 	err := cmd.Start()
 	r.NoError(err)
 
-	cmd2 := exec.Command("offer")
+	cmd2 := exec.Command("./offer_app")
 	var outb2, errb2 bytes.Buffer
 	cmd2.Stdout = &outb2
 	cmd2.Stderr = &errb2
 	err2 := cmd2.Start()
 	r.NoError(err2)
 
-	r.NoError(cmd.Wait())
-	r.NoError(cmd2.Wait())
+	exit1 := cmd.Wait()
 
+	exit2 := cmd2.Wait()
+
+	fmt.Println("out offer:", outb2.String(), "err offer:", errb2.String())
 	fmt.Println("out answer:", outb.String(), "err answer:", errb.String())
-	fmt.Println("out offer:", outb.String(), "err offer:", errb.String())
+
+	r.NoError(exit1)
+	r.NoError(exit2)
 }
